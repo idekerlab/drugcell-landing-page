69f2c8cb5bb807ec475181ffc15811138f911d87
D Otasek <dotasek.dev@gmail.com>
2020-07-03 17:59:58 -0400

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,50 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import Image from '../components/image'
+import './header.css'
+import ccmi from '../images/ccmi2.png'
+
+const Header = ({ siteTitle }) => (
+  <header id='header'>
+    <div id='headerContentDiv'>
+      
+      <h1 id='headerContent'>
+        <a 
+          id='ucsdLink'
+          className='externalLink'
+          href='https://medschool.ucsd.edu/Pages/default.aspx' 
+          target='_blank'
+        >
+          UC San Diego
+        </a> 
+        <a
+          id='hyphenLink'
+          className='externalLink'
+          href='http://idekerlab.ucsd.edu'
+          target='_blank'  
+        >
+          {' - '}
+        </a>
+        <a 
+          id='idekerLink'
+          className='externalLink'
+          href='http://idekerlab.ucsd.edu'
+          target='_blank'
+        >
+          Ideker Lab
+        </a>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+import Layout from '../components/layout'
+import cy3logo from '../images/cy3logoOrange.svg'
+import SEO from '../components/seo'
+import './index.css'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="DrugCell" />
+    <div id='topDivWrapper'>
+      <div id='topDiv'>
+        <div id='topDivContents'>
+          <div id='titlesDiv'>
+            <div id='titleDiv'>
+              DrugCell
+            </div>
+            <div id='subtitleDiv'>
+              Neural Net Predictions of Drug Response
+            </div>
+          </div>
+          <div id='buttonsContainer'>
+            <div id='buttonsDiv'>
+              <div className='buttonDiv'>
+                <form
+                  target='_blank'
+                  action='http://drugcell-dev.ndexbio.org/'
+                >
+                  <button type="submit">
+                    <span className='buttonText'>
+                      Explore Results
+                    </span>
+                  </button>
+                </form>
+              </div>
+              
+              <div className='buttonDiv'>
+                <form
+                  target='_blank' 
+                  action="">
+                  <button type="submit">
+                    <span className='buttonText'>
+                      Application B
+                    </span>
+                  </button>
+                </form>
+              </div>
+              
+              <div className='buttonDiv'>
+                <form
+                  target='_blank' 
+                  action="">
+                  <button type="submit">
+                    <span className='buttonText'>
+                      Application C
+                    </span>
+                  </button>
+                </form>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id='bottomDiv'>
+      <div id='bottomTitle'>
+        <div id='bottomTitleText'>
+          What is DrugCell?
+        </div>
+      </div>
+      <div id='bottomText'>
+        {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+      </div>
+    </div>
+    
+    
+    
+    {/*<h1>Project Name</h1>
+    <p>(You can add any contents here).</p>
+    <p>Now go build something great.</p>
+    <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
+      <img src={cy3logo} />
+    </div>
+    <h2>Main contents</h2>
+    <ul>
+      <li>
+        <Link to="/docs/doc1">Document written in Markdown</Link>
+      </li>
+    </ul>*/}
+  </Layout>
+)
+
+export default IndexPage
