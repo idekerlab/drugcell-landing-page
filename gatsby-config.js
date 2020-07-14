8b578566d7bf9133647cf598361f9c5e329a0631
D Otasek <dotasek.dev@gmail.com>
2020-07-14 10:42:32 -0400

    update path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/nest',
+  pathPrefix: '/drugcell',
   siteMetadata: {
     title: `Ideker Lab Project Page`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
