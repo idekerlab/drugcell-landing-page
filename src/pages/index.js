b1c50d0bbdf69ca07403e514d8e344e24f18ad9a
jisoopark <jp.jisoopark@gmail.com>
2020-11-30 14:34:05 -0800

    Update index.js
    Add additional information about the code availability.
    Tried to adapt the NeST web application style.

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,10 +46,10 @@ const IndexPage = () => (
                   action="http://drugcell.ucsd.edu/analyze/finddrugs/">
                   <button type="submit">
                     <div className='buttonText'>
-                      Analyze Genomes
+                      Find Drugs
                     </div>
                     <div className='buttonCaption'>
-                      Analyze a genome using DrugCell
+                      Analyze a genotype using DrugCell
                     </div>
                   </button>
                 </form>
@@ -61,6 +61,7 @@ const IndexPage = () => (
       </div>
     </div>
 
+  <!-- old version of bottom texts
     <div id='bottomDiv'>
       <div id='bottomTitle'>
         <div id='bottomTitleText'>
@@ -78,7 +79,43 @@ const IndexPage = () => (
         </ul>
       </div>
     </div>
+    -->
+            
+    <div id='bottomDiv'>
+        <Panel backgroundColor='white' textColor='rgba(0, 0, 0, 0.8)'>  
+        <h2>What is DrugCell?</h2>
+        <p> While deep learning framework has been applied to predict cellular response to cancer therapies, 
+            a fundamental problem of being a “black-box” has not yet been fully addressed. To build an interpretable deep learning-based model, 
+            we developed DrugCell, a “visible” neural network (VNN) that predicts anti-cancer drug responses by modeling the hierarchical 
+            organization of a human cancer cell. During the training phase, genotypes and compound structures induce differential patterns of 
+            subsystem activity, enabling in silico investigations of the molecular mechanisms underlying cancer drug response which is crucial 
+            to the novel drug discovery and better implementation of precision medicine.
+        </p>
+        </Panel>
 
+        <Panel backgroundColor={contrastBackgroundColor} textColor={contrastTextColor}>
+        <h2>Code Availability</h2>
+        <p>
+            The codebase for training a DrugCell model along with instructions to set up the required computational environment is published in 
+            a GitHub repository. The Github repository also contains a pre-trained DrugCell model that we used our analysis and example command lines. 
+            The DrugCell GitHub repository can be found at: <br/>
+            <LinkOut link='https://github.com/idekerlab/DrugCell'>
+                https://github.com/idekerlab/DrugCell
+            </LinkOut>
+        </p>
+        </Panel>
+      
+        <Panel backgroundColor={contrastBackgroundColor} textColor={contrastTextColor}>
+        <h2>Publications</h2>
+          <p> Please cite the following when utilizing this application in your study:
+          <ul>
+          <li>Kuenzi BM, Park J, Fong SH, Sanchez KS, Lee J, Kreisberg JF, Ma J, Ideker T. Predicting drug response and synergy using a deep learning model of human cancer cells. Cancer Cell. 2020 Nov 9;38(5):672-84. </li>
+          <li>Ma J, Yu MK, Fong S, Ono K, Sage E, Demchak B, Sharan R, Ideker T. Using deep learning to model the hierarchical structure and function of a cell. Nature methods. 2018 Apr;15(4):290.</li>
+          <li></li>
+          </ul>
+          </p>
+        </Panel>
+    </div>
 
 
     {/*<h1>Project Name</h1>
